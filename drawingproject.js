eb9ac4b0461ef3fa97711f6ce5b41f8ac62dc315
Bryan Campuzano <bryan.campuzano82@gmail.com>
2016-10-10 11:42:22 -0700

    Made some changes
    Drew a Pikachu

---
+++ drawingproject.js
@@ -0,0 +1,504 @@
+var canvasDiv = document.getElementById("drawingArea");
+var canvas = canvasDiv.getContext('2d');
+
+canvas.fillStyle = "black";
+
+canvas.fillRect(0, 0, 20, 20);
+canvas.fillRect(0, 20, 20, 20);
+canvas.fillRect(0, 40, 20, 20);
+canvas.fillRect(0, 60, 20, 20);
+
+canvas.fillRect(20, 0, 20, 20);
+canvas.fillRect(20, 20, 20, 20);
+canvas.fillRect(20, 40, 20, 20);
+canvas.fillRect(20, 60, 20, 20);
+canvas.fillRect(20, 80, 20, 20);
+canvas.fillRect(20, 100, 20, 20);
+
+canvas.fillRect(40, 20, 20, 20);
+canvas.fillRect(40, 40, 20, 20);
+canvas.fillRect(40, 60, 20, 20);
+canvas.fillRect(40, 80, 20, 20);
+canvas.fillRect(40, 100, 20, 20);
+canvas.fillRect(40, 120, 20, 20);
+canvas.fillRect(40, 140, 20, 20);
+
+canvas.fillRect(60, 20, 20, 20);
+canvas.fillRect(60, 160, 20, 20);
+
+canvas.fillRect(80, 40, 20, 20);
+canvas.fillRect(80, 180, 20, 20);
+canvas.fillRect(80, 340, 20, 20);
+canvas.fillRect(80, 360, 20, 20);
+canvas.fillRect(80, 380, 20, 20);
+
+canvas.fillRect(100, 60, 20, 20);
+canvas.fillRect(100, 200, 20, 20);
+canvas.fillRect(100, 260, 20, 20);
+canvas.fillRect(100, 280, 20, 20);
+canvas.fillRect(100, 300, 20, 20);
+canvas.fillRect(100, 320, 20, 20);
+canvas.fillRect(100, 400, 20, 20);
+
+canvas.fillRect(120, 80, 20, 20);
+canvas.fillRect(120, 220, 20, 20);
+canvas.fillRect(120, 240, 20, 20);
+canvas.fillRect(120, 420, 20, 20);
+
+canvas.fillRect(140, 80, 20, 20);
+canvas.fillRect(140, 200, 20, 20);
+canvas.fillRect(140, 440, 20, 20);
+
+canvas.fillRect(160, 100, 20, 20);
+canvas.fillRect(160, 280, 20, 20);
+canvas.fillRect(160, 300, 20, 20);
+canvas.fillRect(160, 440, 20, 20);
+
+canvas.fillRect(180, 120, 20, 20);
+canvas.fillRect(180, 260, 20, 20);
+canvas.fillRect(180, 300, 20, 20);
+canvas.fillRect(180, 320, 20, 20);
+canvas.fillRect(180, 460, 20, 20);
+
+canvas.fillRect(200, 120, 20, 20);
+canvas.fillRect(200, 260, 20, 20);
+canvas.fillRect(200, 280, 20, 20);
+canvas.fillRect(200, 300, 20, 20);
+canvas.fillRect(200, 320, 20, 20);
+canvas.fillRect(200, 460, 20, 20);
+
+canvas.fillRect(220, 140, 20, 20);
+canvas.fillRect(220, 280, 20, 20);
+canvas.fillRect(220, 300, 20, 20);
+canvas.fillRect(220, 460, 20, 20);
+
+canvas.fillRect(240, 140, 20, 20);
+canvas.fillRect(240, 360, 20, 20);
+canvas.fillRect(240, 480, 20, 20);
+
+canvas.fillRect(260, 120, 20, 20);
+canvas.fillRect(260, 380, 20, 20);
+canvas.fillRect(260, 400, 20, 20);
+canvas.fillRect(260, 420, 20, 20);
+canvas.fillRect(260, 480, 20, 20);
+
+canvas.fillRect(280, 120, 20, 20);
+canvas.fillRect(280, 320, 20, 20);
+canvas.fillRect(280, 360, 20, 20);
+canvas.fillRect(280, 380, 20, 20);
+canvas.fillRect(280, 440, 20, 20);
+canvas.fillRect(280, 480, 20, 20);
+
+canvas.fillRect(300, 120, 20, 20);
+canvas.fillRect(300, 320, 20, 20);
+canvas.fillRect(300, 360, 20, 20);
+canvas.fillRect(300, 380, 20, 20);
+canvas.fillRect(300, 440, 20, 20);
+canvas.fillRect(300, 480, 20, 20);
+
+canvas.fillRect(320, 120, 20, 20);
+canvas.fillRect(320, 380, 20, 20);
+canvas.fillRect(320, 400, 20, 20);
+canvas.fillRect(320, 420, 20, 20);
+canvas.fillRect(320, 480, 20, 20);
+
+canvas.fillRect(340, 140, 20, 20);
+canvas.fillRect(340, 360, 20, 20);
+canvas.fillRect(340, 480, 20, 20);
+
+canvas.fillRect(360, 140, 20, 20);
+canvas.fillRect(360, 280, 20, 20);
+canvas.fillRect(360, 300, 20, 20);
+canvas.fillRect(360, 460, 20, 20);
+
+canvas.fillRect(380, 120, 20, 20);
+canvas.fillRect(380, 260, 20, 20);
+canvas.fillRect(380, 300, 20, 20);
+canvas.fillRect(380, 320, 20, 20);
+canvas.fillRect(380, 460, 20, 20);
+
+canvas.fillRect(400, 120, 20, 20);
+canvas.fillRect(400, 260, 20, 20);
+canvas.fillRect(400, 280, 20, 20);
+canvas.fillRect(400, 300, 20, 20);
+canvas.fillRect(400, 320, 20, 20);
+canvas.fillRect(400, 460, 20, 20);
+
+canvas.fillRect(420, 100, 20, 20);
+canvas.fillRect(420, 280, 20, 20);
+canvas.fillRect(420, 300, 20, 20);
+canvas.fillRect(420, 440, 20, 20);
+
+canvas.fillRect(440, 80, 20, 20);
+canvas.fillRect(440, 200, 20, 20);
+canvas.fillRect(440, 440, 20, 20);
+
+canvas.fillRect(460, 80, 20, 20);
+canvas.fillRect(460, 220, 20, 20);
+canvas.fillRect(460, 240, 20, 20);
+canvas.fillRect(460, 420, 20, 20);
+
+canvas.fillRect(480, 60, 20, 20);
+canvas.fillRect(480, 200, 20, 20);
+canvas.fillRect(480, 260, 20, 20);
+canvas.fillRect(480, 280, 20, 20);
+canvas.fillRect(480, 300, 20, 20);
+canvas.fillRect(480, 320, 20, 20);
+canvas.fillRect(480, 400, 20, 20);
+
+canvas.fillRect(500, 40, 20, 20);
+canvas.fillRect(500, 180, 20, 20);
+canvas.fillRect(500, 340, 20, 20);
+canvas.fillRect(500, 360, 20, 20);
+canvas.fillRect(500, 380, 20, 20);
+
+canvas.fillRect(520, 20, 20, 20);
+canvas.fillRect(520, 160, 20, 20);
+
+canvas.fillRect(540, 20, 20, 20);
+canvas.fillRect(540, 40, 20, 20);
+canvas.fillRect(540, 60, 20, 20);
+canvas.fillRect(540, 80, 20, 20);
+canvas.fillRect(540, 100, 20, 20);
+canvas.fillRect(540, 120, 20, 20);
+canvas.fillRect(540, 140, 20, 20);
+
+canvas.fillRect(560, 0, 20, 20);
+canvas.fillRect(560, 20, 20, 20);
+canvas.fillRect(560, 40, 20, 20);
+canvas.fillRect(560, 60, 20, 20);
+canvas.fillRect(560, 80, 20, 20);
+canvas.fillRect(560, 100, 20, 20);
+
+canvas.fillRect(580, 0, 20, 20);
+canvas.fillRect(580, 20, 20, 20);
+canvas.fillRect(580, 40, 20, 20);
+canvas.fillRect(580, 60, 20, 20);
+
+
+canvas.fillStyle = "yellow";
+
+canvas.fillRect(60, 40, 20, 20);
+canvas.fillRect(60, 60, 20, 20);
+canvas.fillRect(60, 80, 20, 20);
+canvas.fillRect(60, 100, 20, 20);
+canvas.fillRect(60, 120, 20, 20);
+canvas.fillRect(60, 140, 20, 20);
+
+canvas.fillRect(80, 60, 20, 20);
+canvas.fillRect(80, 80, 20, 20);
+canvas.fillRect(80, 100, 20, 20);
+canvas.fillRect(80, 120, 20, 20);
+canvas.fillRect(80, 140, 20, 20);
+canvas.fillRect(80, 160, 20, 20);
+
+canvas.fillRect(100, 80, 20, 20);
+canvas.fillRect(100, 100, 20, 20);
+canvas.fillRect(100, 120, 20, 20);
+canvas.fillRect(100, 140, 20, 20);
+canvas.fillRect(100, 160, 20, 20);
+canvas.fillRect(100, 180, 20, 20);
+canvas.fillRect(100, 340, 20, 20);
+canvas.fillRect(100, 360, 20, 20);
+canvas.fillRect(100, 380, 20, 20);
+
+canvas.fillRect(120, 100, 20, 20);
+canvas.fillRect(120, 120, 20, 20);
+canvas.fillRect(120, 140, 20, 20);
+canvas.fillRect(120, 160, 20, 20);
+canvas.fillRect(120, 180, 20, 20);
+canvas.fillRect(120, 200, 20, 20);
+canvas.fillRect(120, 260, 20, 20);
+canvas.fillRect(120, 280, 20, 20);
+canvas.fillRect(120, 300, 20, 20);
+canvas.fillRect(120, 320, 20, 20);
+canvas.fillRect(120, 340, 20, 20);
+canvas.fillRect(120, 360, 20, 20);
+canvas.fillRect(120, 380, 20, 20);
+canvas.fillRect(120, 400, 20, 20);
+
+canvas.fillRect(140, 100, 20, 20);
+canvas.fillRect(140, 120, 20, 20);
+canvas.fillRect(140, 140, 20, 20);
+canvas.fillRect(140, 160, 20, 20);
+canvas.fillRect(140, 180, 20, 20);
+canvas.fillRect(140, 220, 20, 20);
+canvas.fillRect(140, 240, 20, 20);
+canvas.fillRect(140, 260, 20, 20);
+canvas.fillRect(140, 280, 20, 20);
+canvas.fillRect(140, 300, 20, 20);
+canvas.fillRect(140, 320, 20, 20);
+canvas.fillRect(140, 340, 20, 20);
+canvas.fillRect(140, 360, 20, 20);
+canvas.fillRect(140, 380, 20, 20);
+canvas.fillRect(140, 400, 20, 20);
+canvas.fillRect(140, 420, 20, 20);
+
+canvas.fillRect(160, 120, 20, 20);
+canvas.fillRect(160, 140, 20, 20);
+canvas.fillRect(160, 160, 20, 20);
+canvas.fillRect(160, 180, 20, 20);
+canvas.fillRect(160, 200, 20, 20);
+canvas.fillRect(160, 220, 20, 20);
+canvas.fillRect(160, 240, 20, 20);
+canvas.fillRect(160, 260, 20, 20);
+canvas.fillRect(160, 320, 20, 20);
+canvas.fillRect(160, 340, 20, 20);
+canvas.fillRect(160, 360, 20, 20);
+canvas.fillRect(160, 380, 20, 20);
+canvas.fillRect(160, 400, 20, 20);
+canvas.fillRect(160, 420, 20, 20);
+
+canvas.fillRect(180, 140, 20, 20);
+canvas.fillRect(180, 160, 20, 20);
+canvas.fillRect(180, 180, 20, 20);
+canvas.fillRect(180, 200, 20, 20);
+canvas.fillRect(180, 220, 20, 20);
+canvas.fillRect(180, 240, 20, 20);
+canvas.fillRect(180, 340, 20, 20);
+canvas.fillRect(180, 360, 20, 20);
+canvas.fillRect(180, 380, 20, 20);
+canvas.fillRect(180, 400, 20, 20);
+canvas.fillRect(180, 420, 20, 20);
+canvas.fillRect(180, 440, 20, 20);
+
+canvas.fillRect(200, 140, 20, 20);
+canvas.fillRect(200, 160, 20, 20);
+canvas.fillRect(200, 180, 20, 20);
+canvas.fillRect(200, 200, 20, 20);
+canvas.fillRect(200, 220, 20, 20);
+canvas.fillRect(200, 240, 20, 20);
+canvas.fillRect(200, 340, 20, 20);
+canvas.fillRect(200, 360, 20, 20);
+canvas.fillRect(200, 380, 20, 20);
+canvas.fillRect(200, 400, 20, 20);
+canvas.fillRect(200, 420, 20, 20);
+canvas.fillRect(200, 440, 20, 20);
+
+canvas.fillRect(220, 160, 20, 20);
+canvas.fillRect(220, 180, 20, 20);
+canvas.fillRect(220, 200, 20, 20);
+canvas.fillRect(220, 220, 20, 20);
+canvas.fillRect(220, 240, 20, 20);
+canvas.fillRect(220, 260, 20, 20);
+canvas.fillRect(220, 320, 20, 20);
+canvas.fillRect(220, 340, 20, 20);
+canvas.fillRect(220, 360, 20, 20);
+canvas.fillRect(220, 380, 20, 20);
+canvas.fillRect(220, 400, 20, 20);
+canvas.fillRect(220, 420, 20, 20);
+canvas.fillRect(220, 440, 20, 20);
+
+canvas.fillRect(240, 160, 20, 20);
+canvas.fillRect(240, 180, 20, 20);
+canvas.fillRect(240, 200, 20, 20);
+canvas.fillRect(240, 220, 20, 20);
+canvas.fillRect(240, 240, 20, 20);
+canvas.fillRect(240, 260, 20, 20);
+canvas.fillRect(240, 280, 20, 20);
+canvas.fillRect(240, 300, 20, 20);
+canvas.fillRect(240, 320, 20, 20);
+canvas.fillRect(240, 340, 20, 20);
+canvas.fillRect(240, 380, 20, 20);
+canvas.fillRect(240, 400, 20, 20);
+canvas.fillRect(240, 420, 20, 20);
+canvas.fillRect(240, 440, 20, 20);
+canvas.fillRect(240, 460, 20, 20);
+
+canvas.fillRect(260, 140, 20, 20);
+canvas.fillRect(260, 160, 20, 20);
+canvas.fillRect(260, 180, 20, 20);
+canvas.fillRect(260, 200, 20, 20);
+canvas.fillRect(260, 220, 20, 20);
+canvas.fillRect(260, 240, 20, 20);
+canvas.fillRect(260, 260, 20, 20);
+canvas.fillRect(260, 280, 20, 20);
+canvas.fillRect(260, 300, 20, 20);
+canvas.fillRect(260, 320, 20, 20);
+canvas.fillRect(260, 340, 20, 20);
+canvas.fillRect(260, 360, 20, 20);
+canvas.fillRect(260, 440, 20, 20);
+canvas.fillRect(260, 460, 20, 20);
+
+canvas.fillRect(280, 140, 20, 20);
+canvas.fillRect(280, 160, 20, 20);
+canvas.fillRect(280, 180, 20, 20);
+canvas.fillRect(280, 200, 20, 20);
+canvas.fillRect(280, 220, 20, 20);
+canvas.fillRect(280, 240, 20, 20);
+canvas.fillRect(280, 260, 20, 20);
+canvas.fillRect(280, 280, 20, 20);
+canvas.fillRect(280, 300, 20, 20);
+canvas.fillRect(280, 340, 20, 20);
+canvas.fillRect(280, 460, 20, 20);
+
+canvas.fillRect(300, 140, 20, 20);
+canvas.fillRect(300, 160, 20, 20);
+canvas.fillRect(300, 180, 20, 20);
+canvas.fillRect(300, 200, 20, 20);
+canvas.fillRect(300, 220, 20, 20);
+canvas.fillRect(300, 240, 20, 20);
+canvas.fillRect(300, 260, 20, 20);
+canvas.fillRect(300, 280, 20, 20);
+canvas.fillRect(300, 300, 20, 20);
+canvas.fillRect(300, 340, 20, 20);
+canvas.fillRect(300, 460, 20, 20);
+
+canvas.fillRect(320, 140, 20, 20);
+canvas.fillRect(320, 160, 20, 20);
+canvas.fillRect(320, 180, 20, 20);
+canvas.fillRect(320, 200, 20, 20);
+canvas.fillRect(320, 220, 20, 20);
+canvas.fillRect(320, 240, 20, 20);
+canvas.fillRect(320, 260, 20, 20);
+canvas.fillRect(320, 280, 20, 20);
+canvas.fillRect(320, 300, 20, 20);
+canvas.fillRect(320, 320, 20, 20);
+canvas.fillRect(320, 340, 20, 20);
+canvas.fillRect(320, 360, 20, 20);
+canvas.fillRect(320, 440, 20, 20);
+canvas.fillRect(320, 460, 20, 20);
+
+canvas.fillRect(340, 160, 20, 20);
+canvas.fillRect(340, 180, 20, 20);
+canvas.fillRect(340, 200, 20, 20);
+canvas.fillRect(340, 220, 20, 20);
+canvas.fillRect(340, 240, 20, 20);
+canvas.fillRect(340, 260, 20, 20);
+canvas.fillRect(340, 280, 20, 20);
+canvas.fillRect(340, 300, 20, 20);
+canvas.fillRect(340, 320, 20, 20);
+canvas.fillRect(340, 340, 20, 20);
+canvas.fillRect(340, 380, 20, 20);
+canvas.fillRect(340, 400, 20, 20);
+canvas.fillRect(340, 420, 20, 20);
+canvas.fillRect(340, 440, 20, 20);
+canvas.fillRect(340, 460, 20, 20);
+
+canvas.fillRect(360, 160, 20, 20);
+canvas.fillRect(360, 180, 20, 20);
+canvas.fillRect(360, 200, 20, 20);
+canvas.fillRect(360, 220, 20, 20);
+canvas.fillRect(360, 240, 20, 20);
+canvas.fillRect(360, 260, 20, 20);
+canvas.fillRect(360, 320, 20, 20);
+canvas.fillRect(360, 340, 20, 20);
+canvas.fillRect(360, 360, 20, 20);
+canvas.fillRect(360, 380, 20, 20);
+canvas.fillRect(360, 400, 20, 20);
+canvas.fillRect(360, 420, 20, 20);
+canvas.fillRect(360, 440, 20, 20);
+
+canvas.fillRect(380, 140, 20, 20);
+canvas.fillRect(380, 160, 20, 20);
+canvas.fillRect(380, 180, 20, 20);
+canvas.fillRect(380, 200, 20, 20);
+canvas.fillRect(380, 220, 20, 20);
+canvas.fillRect(380, 240, 20, 20);
+canvas.fillRect(380, 340, 20, 20);
+canvas.fillRect(380, 360, 20, 20);
+canvas.fillRect(380, 380, 20, 20);
+canvas.fillRect(380, 400, 20, 20);
+canvas.fillRect(380, 420, 20, 20);
+canvas.fillRect(380, 440, 20, 20);
+
+canvas.fillRect(400, 140, 20, 20);
+canvas.fillRect(400, 160, 20, 20);
+canvas.fillRect(400, 180, 20, 20);
+canvas.fillRect(400, 200, 20, 20);
+canvas.fillRect(400, 220, 20, 20);
+canvas.fillRect(400, 240, 20, 20);
+canvas.fillRect(400, 340, 20, 20);
+canvas.fillRect(400, 360, 20, 20);
+canvas.fillRect(400, 380, 20, 20);
+canvas.fillRect(400, 400, 20, 20);
+canvas.fillRect(400, 420, 20, 20);
+canvas.fillRect(400, 440, 20, 20);
+
+canvas.fillRect(420, 120, 20, 20);
+canvas.fillRect(420, 140, 20, 20);
+canvas.fillRect(420, 160, 20, 20);
+canvas.fillRect(420, 180, 20, 20);
+canvas.fillRect(420, 200, 20, 20);
+canvas.fillRect(420, 220, 20, 20);
+canvas.fillRect(420, 240, 20, 20);
+canvas.fillRect(420, 260, 20, 20);
+canvas.fillRect(420, 320, 20, 20);
+canvas.fillRect(420, 340, 20, 20);
+canvas.fillRect(420, 360, 20, 20);
+canvas.fillRect(420, 380, 20, 20);
+canvas.fillRect(420, 400, 20, 20);
+canvas.fillRect(420, 420, 20, 20);
+
+canvas.fillRect(440, 100, 20, 20);
+canvas.fillRect(440, 120, 20, 20);
+canvas.fillRect(440, 140, 20, 20);
+canvas.fillRect(440, 160, 20, 20);
+canvas.fillRect(440, 180, 20, 20);
+
+canvas.fillRect(440, 220, 20, 20);
+canvas.fillRect(440, 240, 20, 20);
+canvas.fillRect(440, 260, 20, 20);
+canvas.fillRect(440, 280, 20, 20);
+canvas.fillRect(440, 300, 20, 20);
+canvas.fillRect(440, 320, 20, 20);
+canvas.fillRect(440, 340, 20, 20);
+canvas.fillRect(440, 360, 20, 20);
+canvas.fillRect(440, 380, 20, 20);
+canvas.fillRect(440, 400, 20, 20);
+canvas.fillRect(440, 420, 20, 20);
+
+canvas.fillRect(460, 100, 20, 20);
+canvas.fillRect(460, 120, 20, 20);
+canvas.fillRect(460, 140, 20, 20);
+canvas.fillRect(460, 160, 20, 20);
+canvas.fillRect(460, 180, 20, 20);
+canvas.fillRect(460, 200, 20, 20);
+canvas.fillRect(460, 260, 20, 20);
+canvas.fillRect(460, 280, 20, 20);
+canvas.fillRect(460, 300, 20, 20);
+canvas.fillRect(460, 320, 20, 20);
+canvas.fillRect(460, 340, 20, 20);
+canvas.fillRect(460, 360, 20, 20);
+canvas.fillRect(460, 380, 20, 20);
+canvas.fillRect(460, 400, 20, 20);
+
+canvas.fillRect(480, 80, 20, 20);
+canvas.fillRect(480, 100, 20, 20);
+canvas.fillRect(480, 120, 20, 20);
+canvas.fillRect(480, 140, 20, 20);
+canvas.fillRect(480, 160, 20, 20);
+canvas.fillRect(480, 180, 20, 20);
+canvas.fillRect(480, 340, 20, 20);
+canvas.fillRect(480, 360, 20, 20);
+canvas.fillRect(480, 380, 20, 20);
+
+canvas.fillRect(500, 60, 20, 20);
+canvas.fillRect(500, 80, 20, 20);
+canvas.fillRect(500, 100, 20, 20);
+canvas.fillRect(500, 120, 20, 20);
+canvas.fillRect(500, 140, 20, 20);
+canvas.fillRect(500, 160, 20, 20);
+
+canvas.fillRect(520, 40, 20, 20);
+canvas.fillRect(520, 60, 20, 20);
+canvas.fillRect(520, 80, 20, 20);
+canvas.fillRect(520, 100, 20, 20);
+canvas.fillRect(520, 120, 20, 20);
+canvas.fillRect(520, 140, 20, 20);
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.moveTo(450,380);
+canvas.lineTo(554,320);
+canvas.lineTo(550,330);
+canvas.fill();
+
+canvas.beginPath();
+canvas.arc(180, 390, 40, 0, 360);
+canvas.arc(420, 390, 40, 0, 360);
+canvas.fillStyle = "red";
+canvas.fill();
+
+canvas.fillRect(280, 400, 40, 20);
+canvas.fillRect(280, 420, 40, 20);
